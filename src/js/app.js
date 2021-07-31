164496347bc7af850b51bd4bc8c7e958960a9605
Lazy-ferret <a.veselov83@gmail.com>
2021-07-31 23:13:14 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,86 @@
+
+const goblinImg = document.createElement('img');
+goblinImg.src = './img/goblin.png';
+goblinImg.classList.add('goblin-img');
+
+const holes = document.querySelectorAll('.hole');
+
+const hit = document.getElementById('hit');
+let hitCount = 0;
+const miss = document.getElementById('miss');
+let missCount = 0;
+
+const startBtn = document.querySelector('.start-button');
+const resultTitle = document.createElement('h2');
+const parentEl = document.querySelector('.start-title');
+
+let timer;
+
+const position = function getRandom() {
+  return Math.floor(Math.random() * holes.length);
+};
+
+function getTimer() {
+  clearInterval(timer);
+  timer = setInterval(changePosition, 1000)
+};
+
+
+function changePosition() {
+  if (document.images) {
+    const currentImgParent = document.images[0].parentElement;
+    currentImgParent.removeChild(document.images[0]);
+  }
+  holes[position()].insertBefore(goblinImg, holes[position()].firstChild);
+}
+
+function GameStart() {
+  if (resultTitle.textContent) {
+    parentEl.removeChild(resultTitle);
+  }
+  getTimer();
+  hitCount = 0;
+  missCount = 0;
+  hit.textContent = hitCount;
+  miss.textContent = missCount;
+}
+
+function GameStop() {
+  console.log('Game Stopped');
+  clearInterval(timer);
+  parentEl.appendChild(resultTitle);
+}
+
+holes.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    console.log('clicked')
+    e.preventDefault();
+    if (e.target.classList.contains('goblin-img')) {
+      hitCount += 1;
+      console.log(hitCount)
+      hit.textContent = hitCount;
+      if (hitCount === 5) {
+        GameStop()
+        resultTitle.textContent = 'Победа! Поздравляю!'
+      }
+    }
+    else {
+      missCount += 1;
+      miss.textContent = missCount;
+      if (missCount === 5) {
+        GameStop();
+        resultTitle.textContent = 'Ты проиграл! Попробуй еще раз'
+      }
+    }
+  })
+})
+
+startBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  GameStart();
+})
+
+// for demonstration purpose only;
+export default function demo(value) {
+  return value;
+}
